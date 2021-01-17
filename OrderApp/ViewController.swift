c169a080ba00af8646614645632d88c33f986a73
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-01-17 11:45:57 +0900

    Initial Commit

---
+++ OrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderApp
+//
+//  Created by Yuki Tsukada on 2021/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
